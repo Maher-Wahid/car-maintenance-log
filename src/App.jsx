3b00b5b619ce2df6b8f808744b8357e02afff67d
Maher Wahid <mahernama410@gmail.com>
2026-07-21 23:14:44 -0500

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 
 function App() {
-    return <></>;
+    return (
+        <>
+            <h1>Car Maintenance Log</h1>
+        </>
+    );
 }
 
 export default App;
